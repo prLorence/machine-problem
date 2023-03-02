243f4f39d5cad4236c1411e57fb645935cd3110c
prLorence <paulorencereyes@gmail.com>
2023-03-02 21:59:07 +0800

    init commit

---
+++ src/functions/testMath.js
@@ -0,0 +1,11 @@
+import * as math from "mathjs";
+
+
+export default function getNthRoot() {
+  let test = "nthRoot(e)"
+  // console.log(test.indexOf("pi"));
+  // switch(test)
+
+  console.log(`test sqrt pi: ${math.evaluate(test)}`);
+
+}
